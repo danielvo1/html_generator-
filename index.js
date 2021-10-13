e53ded8eade351bdc0d98c817527212f68daea50
Daniel Vo <daniel.vo@berkeley.edu>
2021-10-13 10:52:10 -0700

    removed error catching at the end

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ inquirer
       // const username = answers
     // Use user feedback for... whatever!!
   })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     // Prompt couldn't be rendered in the current environment
+  //   } else {
+  //     // Something else went wrong
+  //   }
+  // }
+  ;
